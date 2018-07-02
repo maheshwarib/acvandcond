6bab90d58183bfbdd0e9ad9b08ca42a9c5ffc8cf
maheshwarib <maheshwari028@gmail.com>
2018-07-02 11:46:28 +0530

    updated the website routing

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config['base_url'] = 'http://localhost/acvandcond/';
 $config['index_page'] = '';
-$config['uri_protocol']	= 'REQUEST_URI';
+$config['uri_protocol']	= 'AUTO';
 $config['url_suffix'] = '';
 $config['language']	= 'english';
 $config['charset'] = 'UTF-8';
--- application/config/routes.php
+++ application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$route['default_controller'] = 'acvandcond';
+$route['default_controller'] = 'Acvandcond';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
